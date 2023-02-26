e02a0f844c5aa97949a85b2a1a795a3772070f55
odige <1905448814@qq.com>
2023-02-26 14:16:48 +0800

    feat:添加登录页

---
+++ src/components/LogIn.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row ms-4">
+        登录
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LogIn',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,15 +5,16 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 // 在此添加路由页面
+import login from "@/components/LogIn.vue";
 import home from "@/components/ManaGe.vue";
 import accomplish from "@/components/AccomPlish.vue";
 import order from "@/components/OrDer.vue";
 import tag from "@/components/TaG.vue";
 
-
 // 在此添加路由
 const routes = [
-  { path: "/", component: home },
+  { path: "/", component: login },
+  { path: "/home", component: home },
   { path: "/accomplish", component: accomplish },
   { path: "/order", component: order },
   { path: "/tag", component: tag },
