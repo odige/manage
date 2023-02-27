60cdfdc61c2f078db03f543773a90bd53d875e72
odige <1905448814@qq.com>
2023-02-27 17:06:24 +0800

    feat:登入页完善

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-dark bg-primary bg-body-tertiary sticky-top bg-primary-border-subtle">
+    <nav class="navbar navbar-dark bg-primary bg-body-tertiary sticky-top bg-primary-border-subtle"
+      v-show="!(path === '/')">
       <div>
 
         <button class="navbar-toggler mx-3 d-block-inline d-md-none " type="button" data-bs-toggle="offcanvas"
           data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <i class="bi bi-list"></i>
         </button>
-        <a class="navbar-brand m-3" href="#">后台管理</a>
+        <a class="navbar-brand m-3" >后台管理</a>
         <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
           id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
@@ -23,7 +24,7 @@
               </div>
               <div class="box d-flex flex-row mt-auto click">
                 <h4 class="text-center icon my-3"><i class="bi bi-indent"></i></h4>
-                <p class="text my-auto ps-3 ">退出登录</p>
+                <p class="text my-auto ps-3 "  @click="to_('')">退出登录</p>
               </div>
             </div>
 
@@ -32,20 +33,25 @@
       </div>
     </nav>
 
-    <div class="bottm flex-row fixed-bottom d-none d-md-block">
-      <div class="side  d-flex flex-column">
-        <div class="box d-flex flex-row position-relative p-1" v-for="view in views" :key="view.name"
-          @click="to_(view.id)">
-          <h4 class="text-center icon my-2 ms-1"><i :class=view.i></i></h4>
-          <p class="text my-auto ps-3 stretched-link click">{{ view.name }}</p>
-        </div>
-        <div class="box d-flex flex-row mt-auto click">
-          <h4 class="text-center icon my-3"><i class="bi bi-indent"></i></h4>
-          <p class="text my-auto ps-3 ">退出登录</p>
+    <div v-show="!(path === '/')">
+      <div class="bottm flex-row fixed-bottom d-none d-md-block">
+        <div class="side  d-flex flex-column">
+          <div class="box d-flex flex-row position-relative p-1" v-for="view in views" :key="view.name"
+            @click="to_(view.id)">
+            <h4 class="text-center icon my-2 ms-1"><i :class=view.i></i></h4>
+            <p class="text my-auto ps-3 stretched-link click">{{ view.name }}</p>
+          </div>
+          <div class="box d-flex flex-row mt-auto click"  @click="to_('')">
+            <h4 class="text-center icon my-3"><i class="bi bi-indent"></i></h4>
+            <p class="text my-auto ps-3 ">退出登录</p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="view">
+    <div class="view" v-show="!(path === '/')">
+      <router-view></router-view>
+    </div>
+    <div v-show="(path === '/')">
       <router-view></router-view>
     </div>
   </div>
@@ -57,6 +63,7 @@ export default {
   name: 'App',
   data() {
     return {
+      path: '',
       views: [{ i: "bi bi-bag-fill", name: "商品管理", id: "home" },
       { i: "bi bi-tag-fill", name: "TAG管理", id: "tag" },
       { i: "bi bi-box2-fill", name: "订单状态", id: "order" },
@@ -68,6 +75,15 @@ export default {
 
       _push(window, id)
 
+    },
+  },
+  mounted() {
+    this.path = this.$route.path
+  },
+
+  watch: {
+    $route(to) {
+      this.path = to.path
     }
   }
 }
@@ -129,10 +145,11 @@ i {
 .d-block {
   display: none;
 }
-.offcanvas{
-  min-width: 220px;
+
+.offcanvas {
+  --bs-offcanvas-width: 210px
 }
-.offcanvas-body{
-  padding: none;
-}
-</style>
+
+.offcanvas-body {
+  padding: 0px;
+}</style>
--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,19 +1,69 @@
 <template>
-    <div class="row ms-4">
-        登录
+    <div id="login">
+        <div class="container">
+            <div class="row">
+                <div class="mx-auto my-auto box text-center">
+
+                    <div class="form-floating mb-5">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">用户名</label>
+                    </div>
+                    <div class="form-floating mb-5">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">密码</label>
+                    </div>
+                    <button type="button" class="btn btn-primary col-12" @click="to('home')">登入</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { _push } from "@/scripts/page";
 export default {
     name: 'LogIn',
-    props: {
-        msg: String
-    }
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        to(id){
+            _push(window, id)
+        }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#login {
+    min-height: 100vh;
+    background-color: #ffffff;
+}
 
+.row {
+    min-height: 100vh;
+}
+
+.box {
+    width: 270px;
+
+}
+.form-control{
+    background-color: #f2f2f2;
+    border: none;
+}
+.form-control:hover{
+    background-color:#e6e6e6;
+}
+.form-control:focus{
+    box-shadow:none;
+    border:2px solid #1976d2
+}
+.form-control:focus ~ label{
+    opacity: 1;
+    color:#1976d2;
+}
 </style>
